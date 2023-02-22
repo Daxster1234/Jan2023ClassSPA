14afdf3110aa22c44c258480f44299011f5c7df3
Dax Meredith <dax.meredith@gmail.com>
2023-02-21 21:42:11 -0500

    Class SPA Work

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,10 @@
 import html from "html-literal";
 
-export default () => html`
+export default (state) => html`
 <section id="jumbotron">
 <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
 <a href="index.html">"Call to Action" "Button"</a>
-</section>`;
+</section>
+
+<h3>Today's Weather</h3>
+<p>Today's weather in ${state.weather.city} is ${state.weather.description}. It is ${state.weather.temp} degrees and feels like ${state.weather.feelsLike}.</p>`;
