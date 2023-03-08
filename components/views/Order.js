8f8768e0c8d0be4c48ea11601dd5448a6db55b7a
Dax Meredith <dax.meredith@gmail.com>
2023-03-07 21:31:48 -0500

    Finished SPA

--- components/views/Order.js
+++ components/views/Order.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-<section id="order">
+<section>
 <form id="order" method="POST" action="">
   <h2>Order a pizza</h2>
   <div>
@@ -81,7 +81,7 @@ export default () => html`
     type="hidden"
     name="customer"
     id="customer"
-    value="Anonymous Customer"
+    value="Dax M"
   />
   <input type="submit" name="submit" value="Submit Pizza" />
 </form>
